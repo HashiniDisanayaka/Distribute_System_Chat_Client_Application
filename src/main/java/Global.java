c730b6a616c9b9b1c48acf2e5a3ffd7f2812ca93
Hashini Disanayaka <hashinidisanayaka10@gmail.com>
2022-03-21 15:02:43 +0530

    server implementation before implementing the heartbeat and leader election

---
+++ src/main/java/Global.java
@@ -0,0 +1,10 @@
+public abstract class Global {
+
+    private Global(){}
+
+    public static final String CONSENSUS = "Consensus".toUpperCase();
+    public static final String CONSENSUS_TRIGGER = "ConsensusTrigger".toUpperCase();
+    public static final String GOSSIP = "Gossip".toUpperCase();
+    public static final String GOSSIP_TRIGGER = "GossipTrigger".toUpperCase();
+
+}
--- src/main/java/data/ServerState.java
+++ src/main/java/data/ServerState.java
@@ -1,11 +1,13 @@
 package data;
 
-import ChatServer.Server;
+import chatServer.Room;
+import chatServer.Server;
+import chatServer.thredHandler.ClientThreadHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
-import java.util.*;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
@@ -16,10 +18,14 @@ public class ServerState {
     private int client_port;
     private int coordination_port;
     private int serverIdentity;
+    private int numOfPriorServers;
 
     private static ServerState stateInstance;
+    private Room mainHall;
 
     private final ConcurrentHashMap<Integer, Server> setOfservers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Room> setOfRooms = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Long, ClientThreadHandler> setOfClientThreadHandlers = new ConcurrentHashMap<>();
 
     private ServerState () {}
 
@@ -34,8 +40,8 @@ public class ServerState {
         return (stateInstance);
     }
 
-    public void initializeServer (String serverId, String path){
-        this.serverid = serverId;
+    public void initializeServer (String serverid, String path){
+        this.serverid = serverid;
 
         try{
             File file = new File(path);
@@ -47,13 +53,15 @@ public class ServerState {
                 int portClient = Integer.parseInt(dataArray[2]);
                 int portCoordination = Integer.parseInt(dataArray[3]);
                 int serverValue = Integer.parseInt(dataArray[0].substring(1, 2));
-                if (dataArray[0].equals(serverId)) {
+                if (dataArray[0].equals(serverid)) {
                     this.server_address = serverAdd;
                     this.client_port = portClient;
                     this.coordination_port = portCoordination;
                     this.serverIdentity = serverValue;
-                    System.out.println(serverId);
+                    System.out.println(serverIdentity);
+                    System.out.println(server_address);
                     System.out.println(coordination_port);
+                    System.out.println(client_port);
                 }
 
                 Server server = new Server(serverValue, serverAdd, portClient, portCoordination);
@@ -62,12 +70,37 @@ public class ServerState {
             }
             readFile.close();
         } catch (FileNotFoundException e) {
-            System.out.println("ERR : Config file not found");
+            System.out.println("ERR | Config file not found");
             log.error("Config file cannot find : " , e);
             e.printStackTrace();
         }
+        numOfPriorServers = setOfservers.size() - serverIdentity;
+//        System.out.println(numOfPriorServers);
+        this.mainHall = new Room("", getMainHallId(), serverIdentity);
+        this.setOfRooms.put(getMainHallId(), mainHall);
+    }
 
+    public String getMainHallId() {
+        return getMainHallIdbyServerInt(this.serverIdentity);
+    }
 
+    public static String getMainHallIdbyServerInt(int serverIdentity) {
+        return "MainHall-s" + serverIdentity;
+    }
 
+    public String getServer_address() {
+        return server_address;
+    }
+
+    public int getClient_port() {
+        return client_port;
+    }
+
+    public int getCoordination_port () {
+        return coordination_port;
+    }
+
+    public void addClientThreadHandler(ClientThreadHandler clientThreadHandler) {
+        setOfClientThreadHandlers.put(clientThreadHandler.getId(), clientThreadHandler);
     }
 }
